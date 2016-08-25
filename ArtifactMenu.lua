d022ce05f71654f9efa4f48e15d1a2c5ebad2bed
Kevin Gilbert <kgilbert.cmu@gmail.com>
2016-08-25 15:00:17 -0700

    add full addon name as slash command

--- ArtifactMenu.lua
+++ ArtifactMenu.lua
@@ -1,4 +1,5 @@
-LASH_ARTIFACTMENU1, SLASH_ARTIFACTMENU2 = '/artifact', '/am';
+SLASH_ARTIFACTMENU1, SLASH_ARTIFACTMENU2 = '/artifact', '/am';
+SLASH_ARTIFACTMENU3 = "/artifactmenu";
 
 local function openWeaponSockets()
     SocketInventoryItem(16);
